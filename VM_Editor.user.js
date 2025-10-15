2a9081a9d1f92bc14c7bdaa9d7f208878618dca0
jinks908 <claytonjinks@gmail.com>
2025-10-15 16:03:28 -0400

    Minor changes

--- VM_Editor.user.js
+++ VM_Editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Violentmonkey Editor Styles
 // @namespace    SkyColtNinja/userscripts
-// @version      1.1.1
+// @version      1.1.2
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/VM_Editor.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/VM_Editor.user.js
 // @description  Increase font size in Violentmonkey editor
