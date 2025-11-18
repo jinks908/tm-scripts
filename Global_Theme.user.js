85324b7340a2a2c1822b999b69c06ea5923aa584
jinks908 <34957569+jinks908@users.noreply.github.com>
2025-11-18 09:31:58 -0500

    Hide Nord tracking shield icon on Google search

--- Global_Theme.user.js
+++ Global_Theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Global Theme
 // @namespace    SkyColtNinja/userscripts
-// @version      1.1.1
+// @version      1.1.2
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/Global_Theme.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/Global_Theme.user.js
 // @description  Global styles/colors for Firefox
@@ -14,6 +14,9 @@
 (function() {
     'use strict';
 
-   GM_addStyle('html {::selection {background-color: #bcd788 !important; color: #303030 !important;}');
+    // Custom Selection Colors
+    GM_addStyle('html {::selection {background-color: #bcd788 !important; color: #303030 !important;}');
+    // Hide Nord tracking shield icon on Google search results
+    GM_addStyle('span[name="tp-shield"] {display: none !important;}');
 
 })();
