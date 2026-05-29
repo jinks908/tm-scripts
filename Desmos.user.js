edee102d25c91d9495350ef0f3f19decf8396b38
jinks908 <claytonjinks@gmail.com>
2026-05-29 17:28:59 -0400

    Fix: Wait for UI elements to fully load/populate before execution

--- Desmos.user.js
+++ Desmos.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Desmos Enhancements
 // @namespace    SkyColtNinja/userscripts
-// @version      1.0.0
+// @version      1.0.1
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/Desmos.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/Desmos.user.js
 // @description  Enhancements for Desmos graphing calculator
 // @author       SkyColtNinja
-// @match        https://www.desmos.com/calculator/*
+// @match        https://www.desmos.com/calculator*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @run-at       document-idle
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -19,33 +20,34 @@
         el.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
         el.dispatchEvent(new MouseEvent('mouseup',   { bubbles: true }));
         el.dispatchEvent(new MouseEvent('click',     { bubbles: true }));
-    };
+    }
 
-    function waitForElement(selector, callback) {
-        const el = document.querySelector(selector);
-        if (el) { callback(el); return; };
-
-        const observer = new MutationObserver(() => {
+    function waitForElement(selector) {
+        return new Promise((resolve) => {
             const el = document.querySelector(selector);
-            if (el) {
-                observer.disconnect();
-                callback(el);
-            };
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
-    };
+            if (el) { resolve(el); return; }
 
-    function toggleDarkMode() {
-        // Step 1: wait for the wrench to appear, then click it
-        waitForElement('i.dcg-icon-wrench', (wrench) => {
-            clickElement(wrench);
-
-            // Step 2: wait for the settings panel to render, then click the checkbox
-            waitForElement('label.dcg-reverse-contrast-checkbox .dcg-checkbox__box', (checkbox) => {
-                clickElement(checkbox);
+            const observer = new MutationObserver(() => {
+                const el = document.querySelector(selector);
+                if (el) {
+                    observer.disconnect();
+                    resolve(el);
+                }
             });
+            observer.observe(document.body, { childList: true, subtree: true });
         });
-    };
+    }
+
+    async function toggleDarkMode() {
+        const wrench = await waitForElement('i.dcg-icon-wrench');
+
+        clickElement(wrench);
+        // Give Desmos time to finish its initial render before interacting
+        await new Promise(r => setTimeout(r, 1000));
+
+        const checkbox = await waitForElement('label.dcg-reverse-contrast-checkbox .dcg-checkbox__box');
+        clickElement(checkbox);
+    }
 
     toggleDarkMode();
 
