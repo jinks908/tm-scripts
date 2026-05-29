3c51d0b39ad1dce9099d22daf59c02cf335b65e4
jinks908 <claytonjinks@gmail.com>
2026-05-29 17:34:24 -0400

    Refactor/clean up

--- Desmos.user.js
+++ Desmos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Desmos Enhancements
 // @namespace    SkyColtNinja/userscripts
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/Desmos.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/Desmos.user.js
 // @description  Enhancements for Desmos graphing calculator
@@ -16,39 +16,48 @@
 (function() {
     'use strict';
 
+    // Simulate mouse click event
     function clickElement(el) {
         el.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
         el.dispatchEvent(new MouseEvent('mouseup',   { bubbles: true }));
         el.dispatchEvent(new MouseEvent('click',     { bubbles: true }));
-    }
+    };
 
+    // Wait for an element to appear in the DOM
     function waitForElement(selector) {
         return new Promise((resolve) => {
             const el = document.querySelector(selector);
-            if (el) { resolve(el); return; }
+            if (el) { resolve(el); return; };
 
             const observer = new MutationObserver(() => {
                 const el = document.querySelector(selector);
                 if (el) {
                     observer.disconnect();
                     resolve(el);
-                }
+                };
             });
             observer.observe(document.body, { childList: true, subtree: true });
         });
-    }
+    };
 
+    // Navigate the settings menu to toggle reverse contrast (dark mode) on/off
     async function toggleDarkMode() {
         const wrench = await waitForElement('i.dcg-icon-wrench');
 
+        // Open settings menu
         clickElement(wrench);
-        // Give Desmos time to finish its initial render before interacting
-        await new Promise(r => setTimeout(r, 1000));
 
+        // Let settings menu load before checking dark mode checkbox
+        await new Promise(r => setTimeout(r, 1000));
         const checkbox = await waitForElement('label.dcg-reverse-contrast-checkbox .dcg-checkbox__box');
         clickElement(checkbox);
-    }
 
+        // Wait for dark mode switch before closing settings menu
+        await new Promise(r => setTimeout(r, 1000));
+        clickElement(wrench);
+    };
+
+    // Run initial toggle
     toggleDarkMode();
 
 })();
