8adb5100370fb26bb7a21e74522d80f2cf725ea3
jinks908 <claytonjinks@gmail.com>
2026-07-11 13:18:19 -0400

    Add: More code fonts for VimThemes

--- VimThemes.user.js
+++ VimThemes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vim Colorschemes Styles
 // @namespace    SkyColtNinja/userscripts
-// @version      1.0.0
+// @version      1.0.1
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/VimThemes.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/VimThemes.user.js
 // @description  Font and Styles for Vim Colorschemes
@@ -15,8 +15,35 @@
     'use strict';
 
     GM_addStyle(`
-        div.index-module__nCnU7W__content {
-            font-family: 'Victor Mono', monospace !important;
+        @font-face {
+            font-family: 'JetBrains Mono';
+            font-style: normal;
+            font-display: auto;
+            font-weight: 400;
+            src: url(https://cdn.jsdelivr.net/fontsource/fonts/jetbrains-mono@latest/latin-400-normal.woff2) format('woff2'),
+                url(https://cdn.jsdelivr.net/fontsource/fonts/jetbrains-mono@latest/latin-400-normal.woff) format('woff');
+            unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;
+        }
+        @font-face {
+            font-family: 'Source Code Pro';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;
+            src: url(https://cdn.jsdelivr.net/fontsource/fonts/source-code-pro@latest/latin-400-normal.woff2) format('woff2'),
+                url(https://cdn.jsdelivr.net/fontsource/fonts/source-code-pro@latest/latin-400-normal.woff) format('woff');
+            unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;
+        }
+        @font-face {
+            font-family: 'Fira Code';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;
+            src: url(https://cdn.jsdelivr.net/fontsource/fonts/fira-code@latest/latin-400-normal.woff2) format('woff2'),
+                url(https://cdn.jsdelivr.net/fontsource/fonts/fira-code@latest/latin-400-normal.woff) format('woff');
+            unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;
+        }
+        article > div > pre > code {
+            font-family: 'Source Code Pro', monospace !important;
         }
     `);
 
