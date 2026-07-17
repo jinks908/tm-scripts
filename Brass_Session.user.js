bf8c92a25c3e4ee4974cc8353f82e27a4e473c1e
jinks908 <34957569+jinks908@users.noreply.github.com>
2026-07-17 11:22:28 -0400

    Move Brass Styles to Brass Session / Setup server ping for timeout prevention

---
+++ Brass_Session.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Brass Session
+// @namespace    SkyColtNinja/userscripts
+// @version      1.0.0
+// @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/Brass_Session.user.js
+// @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/Brass_Session.user.js
+// @description  Ping the server at regular intervals to remain logged in
+// @author       SkyColtNinja
+// @match        https://jinksperspective.brilliantassessments.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+(function() {
+
+    setInterval(() => {
+        document.dispatchEvent(new MouseEvent('mousemove', {
+            bubbles: true,
+            clientX: Math.floor(Math.random() * window.innerWidth),
+            clientY: Math.floor(Math.random() * window.innerHeight)
+        }));
+
+        document.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, key: 'Shift' }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, key: 'Shift' }));
+    }, 300 * 1000); // 5 minutes
+
+})();
