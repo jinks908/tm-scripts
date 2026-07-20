25064117e2c1da290c310ba42903fadc8b5b9831
jinks908 <34957569+jinks908@users.noreply.github.com>
2026-07-20 10:35:56 -0400

    Fix: Segmentation color picker automation (working version)

--- Brass_Functions.user.js
+++ Brass_Functions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brass Functions
 // @namespace    SkyColtNinja/userscripts
-// @version      1.0.1
+// @version      1.1.0
 // @updateURL    https://raw.githubusercontent.com/jinks908/tm-scripts/main/Brass_Functions.user.js
 // @downloadURL  https://raw.githubusercontent.com/jinks908/tm-scripts/main/Brass_Functions.user.js
 // @description  Custom functions/automations for Brilliant Assessment Builder
@@ -16,6 +16,7 @@
 (function() {
     'use strict';
 
+    // Brand colors
     const colorsHex = {
         'pacific-blue': '#1e4264',
         'cardinal-red': '#c4403f',
@@ -24,39 +25,45 @@
         'deep-pacific': '#2b3843',
         'smoke-blue':   "#4b6471",
     }
-
     const colorsRGB = {
-        'hsv-color-blue': 'rgb(0, 131, 255)',
-        'pacific-blue': 'rgb(30, 66, 100)',
-        'cardinal-red': 'rgb(196, 64, 63)',
-        'sage':         'rgb(95, 136, 111)',
-        'cool-mint':    'rgb(163, 198, 180)',
-        'deep-pacific': 'rgb(43, 56, 67)',
-        'smoke-blue':   'rgb(75, 100, 113)',
+        'pacific-blue':   'rgb(30, 66, 100)',
+        'cardinal-red':   'rgb(196, 64, 63)',
+        'sage':           'rgb(95, 136, 111)',
+        'cool-mint':      'rgb(163, 198, 180)',
+        'deep-pacific':   'rgb(43, 56, 67)',
+        'smoke-blue':     'rgb(75, 100, 113)',
+    }
+
+    function setNativeValue(element, value) {
+        const setter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+        setter.call(element, value);
+        element.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+
+    function getVisibleHexInput() {
+        const inputs = Array.from(document.querySelectorAll('input.k-color-value'));
+        return inputs.find(el => el.offsetParent !== null);
     }
 
     function setColorPicker(color) {
-        const slider = document.querySelector('.k-animation-container .k-slider .k-draghandle');
-        const pickerBox = document.querySelector('.k-animation-container .k-selected-color .k-selected-color-display');
-        const hsvBox = document.querySelector('.k-animation-container .k-hsv-rectangle');
-        const picker = document.querySelector('.k-draghandle');
-
-        if (slider && picker) {
-            picker.style.left = '175px';
-            picker.style.top = '109.41px';
-            pickerBox.style.backgroundColor = colorsRGB['pacific-blue'];
-            hsvBox.style.backgroundColor = colorsRGB['hsv-color-blue'];
-            slider.style.left = '123px';
-            slider.setAttribute('aria-valuenow', '209.14');
-            slider.setAttribute('aria-valuetext', '209.14');
+        const hexInput = getVisibleHexInput();
+        if (!hexInput) {
+            console.error('No visible hex input found.');
+            return;
         }
+
+        hexInput.focus();
+        setNativeValue(hexInput, color);
+
+        hexInput.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', keyCode: 13, bubbles: true }));
+        hexInput.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', keyCode: 13, bubbles: true }));
     }
 
     // Keybindings
     document.addEventListener('keydown', function(e) {
         if (e.ctrlKey && e.key === 'e') {
             e.preventDefault();
-            setColorPicker();
+            setColorPicker(colorsHex['pacific-blue']);
         };
     });
 
